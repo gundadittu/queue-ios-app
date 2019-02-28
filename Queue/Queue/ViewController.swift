ad69fdd6c7b8d53b934bd6d657eb4682a99d3eb7
gundadittu <gundadittu@gmail.com>
2019-02-28 10:41:48 -0600

    first setup

---
+++ Queue/Queue/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Queue
+//
+//  Created by Aditya Gunda on 2/28/19.
+//  Copyright © 2019 Aditya Gunda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
